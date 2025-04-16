62754456cc1001a22859924fdbbed541a43fedc1
sinarfajaroffset <sinarfajar@gmail.com>
2025-04-16 16:47:13 +0700

    Initial commit - Sinar Fajar Offset

---
+++ src/HomePage.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-blue-600">Selamat Datang di Sinar Fajar Offset!</h1>
+      <p className="mt-4 text-gray-700">Ini adalah halaman homepage sederhana.</p>
+    </div>
+  );
+}
